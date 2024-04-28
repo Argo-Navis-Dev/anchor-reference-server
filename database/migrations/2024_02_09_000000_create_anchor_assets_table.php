8507e18b25df46a6e181d8eb76aa616789757a1d
ngybnc <bence91@gmail.com>
2024-04-28 12:40:29 +0300

    fix: db. column type

--- database/migrations/2024_02_09_000000_create_anchor_assets_table.php
+++ database/migrations/2024_02_09_000000_create_anchor_assets_table.php
@@ -20,13 +20,13 @@ return new class extends Migration {
             $table->float('deposit_fee_percent')->nullable();
             $table->float('deposit_fee_minimum')->nullable();
             $table->float('deposit_min_amount')->nullable();
-            $table->float('deposit_max_amount')->nullable();
+            $table->float('deposit_max_amount', 10, 2)->nullable();
             $table->boolean('withdrawal_enabled')->default(true);
             $table->float('withdrawal_fee_fixed')->nullable();
             $table->float('withdrawal_fee_percent')->nullable();
             $table->float('withdrawal_fee_minimum')->nullable();
             $table->float('withdrawal_min_amount')->nullable();
-            $table->float('withdrawal_max_amount')->nullable();
+            $table->float('withdrawal_max_amount', 10, 2)->nullable();
             $table->integer('significant_decimals')->default(2);
             $table->string('schema'); // stellar or iso4217
             $table->boolean('sep24_enabled')->default(true);
